c81e9b41e2e910744b5701cf149f9490bd8ccd02
WillMann94 <william_manning7@yahoo.com>
2020-11-21 16:22:13 -0500

    coffee

---
+++ JavaScript-Basics/05-conditionals.js
@@ -0,0 +1,27 @@
+//1
+var isOn = true;
+
+//2   //3
+if(isOn === true) {
+    console.log("The light is on."); //4
+}
+var isOn = true;
+    //1
+if(isOn) { // same as if (isOn === true) 
+    console.log("The light is on. It's bright.");
+}
+var weather = 75;
+
+if(weather > 70){
+    console.log("Wear shorts today! It's going to be hot!");
+}
+var isOn = true;
+
+if(isOn === true) {
+    console.log("Caveman need food");
+}
+var episodes = 10;
+
+if(episodes > 9){
+    console.log("I'm sad this season is ending");
+}
